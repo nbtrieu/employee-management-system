accdd4ba17cc9345764c7353ebd64fc276348e61
Nicole T <nbtrieu@uci.edu>
2022-12-17 05:02:37 -0800

    Write and debug functions for adding employees and updating role

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { prompt } = require('inquirer');
 const db = require('./db'); // requiring the new Db object exported from the index.js in db folder
 const logo = require('asciiart-logo');
+const { insertEmployee } = require('./db');
 
 require('console.table');
 
@@ -61,10 +62,10 @@ function showMenu() {
         addRoles();
         break;
       case 'Add an employee':
-        addEmoloyees();
+        addEmployees();
         break;
       case 'Update an employee role':
-        updateEmployeeRoles();
+        updateEmployeeRole();
         break;
       default:
         exitApp();
@@ -176,7 +177,7 @@ function addDepartments() {
     db.insertDepartment(userInput.newDepartmentName)
   })
   .then(() => console.log('New department successfully added!'))
-  .then(() => viewDepartments());
+  .then(() => showMenu());
 };
 
 function addRoles() {
@@ -211,12 +212,139 @@ function addRoles() {
       db.insertRole(userInput);
     })
     .then(() => console.log('New role successfully added!'))
-    .then(() => viewRoles());
-  })
-  
-}
+    .then(() => showMenu());
+  });
+};
+
+function addEmployees() {
+  prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: "What is the new employee's first name?"
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: "What is the new employee's last name?"
+    }
+  ])
+  .then(userInput => {
+    let firstName = userInput.first_name;
+    let lastName = userInput.last_name;
+
+    db.selectAllRoles()
+    .then(([rows]) => {
+      let roles = rows;
+      const roleChoices = roles.map(({ id, title }) => ({
+        name: title,
+        value: id
+      }));
+
+      prompt([
+        {
+          type: 'list',
+          name: 'roleId',
+          message: "What is the new employee's role?",
+          choices: roleChoices
+        }
+      ])
+      .then(userChoice => {
+        let roleId = userChoice.roleId;
+
+        db.selectAllEmployees()
+        .then(([rows]) => {
+          let employees = rows;
+          let managerChoices = [];
+          const employeeArray = employees.map(({ id, first_name, last_name, manager }) => ({
+            name: `${first_name} ${last_name}`,
+            manager: manager,
+            value: id 
+          }));
+          for (let i = 0; i < employeeArray.length; i++) {
+            if (employeeArray[i].manager === null) {
+              managerChoices.push(employeeArray[i]);
+            }
+          }
+          managerChoices.unshift({ name: 'None', value: null });
+
+          prompt([
+            {
+              type: 'list',
+              name: 'managerId',
+              message: "Who is the new employee's manager?",
+              choices: managerChoices
+            }
+          ])
+          .then((userChoice) => {
+            let managerId = userChoice.managerId;
+
+            let newEmployee = {
+              first_name: firstName,
+              last_name: lastName,
+              role_id: roleId,
+              manager_id: managerId
+            }
+
+            db.insertEmployee(newEmployee);
+          })
+          .then(() => console.log('New employee successfully added!'))
+          .then(() => showMenu());
+        });
+      });
+    });
+  });
+};
+
+function updateEmployeeRole() {
+  db.selectAllEmployees()
+  .then(([rows]) => {
+    let employees = rows;
+    const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+      name: `${first_name} ${last_name}`,
+      value: id
+    }));
+
+    prompt([
+      {
+        type: 'list',
+        name: 'employeeId',
+        message: 'For which employee would you like to update role?',
+        choices: employeeChoices
+      }
+    ])
+    .then((userChoice) => {
+      let employeeId = userChoice.employeeId;
+
+      db.selectAllRoles()
+      .then(([rows]) => {
+        let roles = rows;
+        const roleChoices = roles.map(({ id, title }) => ({
+          name: title,
+          value: id
+        }));
+
+        prompt([
+          {
+            type: 'list',
+            name: 'roleId',
+            message: "Which role would you like to assign the selected employee?",
+            choices: roleChoices
+          }
+        ])
+        .then((userChoice) => {
+          let roleId = userChoice.roleId;
+
+          db.updateEmployeeRole(employeeId, roleId)
+        })
+        .then(() => console.log("Employee's role successfully updated!"))
+        .then(() => showMenu());
+      });
+    });
+  });
+};
 
 function exitApp() {
-  console.log('You are now exiting the Employee Manager System. Goodbye~');
+  console.log('Now exiting the Employee Manager System. Goodbye~');
   process.exit();
-}
+};
